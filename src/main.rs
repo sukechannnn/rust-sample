d7d67c51ab6f64f45562fc4856d2134fddbcf615
sukechannnn <wonderfulworld0315@gmail.com>
2022-09-19 21:56:24 +0900

    Box

--- src/main.rs
+++ src/main.rs
@@ -119,4 +119,13 @@ fn main() {
     if let Some(num) = vec.get(0) {
         println!("vec: {}", num);
     }
+
+    // Box
+    fn print(s: Box<[u8]>) {
+        println!("{:?}", s)
+    }
+    let byte_array = [b'h', b'e', b'l', b'l', b'o'];
+    print(Box::new(byte_array));
+    let byte_array = [b'w', b'o', b'r', b'l', b'd', b'!'];
+    print(Box::new(byte_array));
 }
