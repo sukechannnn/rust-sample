8fdd5341cf7ee0a7b6623021e33e10e7cd6f6493
sukechannnn <wonderfulworld0315@gmail.com>
2022-09-19 17:07:36 +0900

    Option, Result

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
+    // String
     let s1: String = String::from("Hello, World!");
     let s2: &str = &s1;
     println!("{s1}");
     println!("{s2}");
 
+    // tuple
     let mut t = (1, "2");
     t.0 = 2;
     t.1 = "3";
@@ -14,6 +16,7 @@ fn main() {
     a[1] = b[1];
     println!("{:?}", &a[0..3]);
 
+    // struct
     struct Person {
         name: String,
         age: u32,
@@ -25,6 +28,7 @@ fn main() {
     println!("{:?}", format!("{}: {}", "person", person.name));
     println!("{:?}", format!("{}: {}", "age", person.age));
 
+    // Enum
     #[derive(Debug)]
     enum Event {
         Quit,
@@ -49,4 +53,35 @@ fn main() {
     }
     println!("{:?}", witch_event(Event::KeyDown(3)));
     println!("{:?}", witch_event(Event::KeyDown(4)));
+
+    // Option
+    fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
+        if divisor == 0 {
+            // Failure is represented as the `None` variant
+            None
+        } else {
+            // Result is wrapped in a `Some` variant
+            Some(dividend / divisor)
+        }
+    }
+    println!("{}", checked_division(4, 2).unwrap());
+    println!("{}", checked_division(4, 0).unwrap_or(-1));
+
+    fn try_division(dividend: i32, divisor: i32) {
+        // `Option` values can be pattern matched, just like other enums
+        match checked_division(dividend, divisor) {
+            None => println!("{} / {} failed!", dividend, divisor),
+            Some(quotient) => {
+                println!("{} / {} = {}", dividend, divisor, quotient)
+            }
+        }
+    }
+    try_division(4, 2);
+    try_division(2, 0);
+
+    // Result
+    let result: Result<i32, String> = Ok(200);
+    println!("code: {}", result.unwrap_or(-1));
+    let result: Result<i32, String> = Err("error".to_string());
+    println!("code: {}", result.unwrap_or(-1));
 }
