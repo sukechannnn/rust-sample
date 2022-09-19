a7fd5e72f7abb0a6a6d00732fc7e943bfbaafd75
sukechannnn <wonderfulworld0315@gmail.com>
2022-09-19 10:37:52 +0900

    enum

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,33 @@ fn main() {
     }
     let person = Person {
         name: String::from("John"),
-        age: 8
+        age: 8,
     };
     println!("{:?}", format!("{}: {}", "person", person.name));
     println!("{:?}", format!("{}: {}", "age", person.age));
-}
 
+    #[derive(Debug)]
+    enum Event {
+        Quit,
+        KeyDown(u32),
+        MouseDown { x: i32, y: i32 },
+    }
+    let e1 = Event::Quit;
+    let e2: Event = Event::KeyDown(3);
+    let e3: Event = Event::MouseDown { x: 10, y: 20 };
+    println!("{:?}", e1);
+    println!("{:?}", e2);
+    println!("{:?}", e3);
+
+    fn witch_event(event: Event) -> String {
+        match event {
+            Event::Quit => "quit".to_string(),
+            Event::KeyDown(3) => "3".to_string(),
+            Event::KeyDown(x) => x.to_string(),
+            Event::MouseDown { x: 10, y: 10 } => "10".to_string(),
+            _ => unreachable!(),
+        }
+    }
+    println!("{:?}", witch_event(Event::KeyDown(3)));
+    println!("{:?}", witch_event(Event::KeyDown(4)));
+}
